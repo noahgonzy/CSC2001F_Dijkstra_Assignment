c6b7252033d29732889f775edf94ab309794483a
Noah Gonsenhauser <noahgonzy@gmail.com>
2023-04-25 10:55:19 +0200

    Finished Graph Generator and printing to console

--- src/GenerateGraphs.java
+++ src/GenerateGraphs.java
@@ -7,8 +7,7 @@ public class GenerateGraphs {
 
     public static void GenerateNewGraphs(){
         Random randc = new Random();
-        Random randStart = new Random();
-        Random randEnd = new Random();
+        Random randNode = new Random();
         ArrayList<String> vNodes = new ArrayList<>();
         ArrayList<String> eNodes = new ArrayList<>();
         int cost;
@@ -21,47 +20,43 @@ public class GenerateGraphs {
         boolean addnow = false;;
 
         for(int i = 0; i < 5; i++){
-            for(int j = 0; j < vSize[i]; j++){
-                vNodes.add(""+j);
-                System.out.println(j);
-            }
-            System.out.println("");
-            for(int l = 0; l < vSize[i]-1; l++){
-                cost = 1 + randc.nextInt(8);
-                eNodes.add(l + " " + (l+1) + " " + cost);
-                System.out.println(l + " " + (l+1) + " " + cost);
-            }
-            System.out.println("");
-            k = 0;
-            while(k < eSize[i]-vSize[i]+1){
-                cost = 1 + randc.nextInt(8);
-                start = randStart.nextInt(vSize[i]);
-                end = randStart.nextInt(vSize[i]);
-                if((start != end) && !(start > end)){
-                    addnow = true;
-                    toadd = start + " " + end + " " + cost;
-                    for(int m = 0; m < eNodes.size(); m ++){
-                        addcheck = toadd.substring(0, 3);
-                        addcheckm = eNodes.get(m).substring(0, 3);
-                        if(addcheck.equals(addcheckm)){
-                            addnow = false;
+            for(int o = 0; o < 5; o++){
+                for(int j = 0; j < vSize[i]; j++){
+                    vNodes.add(""+j);
+                }
+                System.out.println("");
+                k = 0;
+                while(k < eSize[o]){
+                    cost = 1 + randc.nextInt(9);
+                    start = randNode.nextInt(vSize[i]);
+                    end = randNode.nextInt(vSize[i]);
+                    if((start != end)){
+                        addnow = true;
+                        toadd = start + " " + end + " " + cost;
+                        for(int m = 0; m < eNodes.size(); m ++){
+                            addcheck = toadd.substring(0, 3);
+                            addcheckm = eNodes.get(m).substring(0, 3);
+                            if(addcheck.equals(addcheckm)){
+                                addnow = false;
+                                break;
+                            }
+                        }
+                        if(addnow){
+                            eNodes.add(toadd);
+                            k++;
                         }
                     }
-                    if(addnow){
-                        eNodes.add(toadd);
-                        k++;
+                    else{
+                        continue;
                     }
                 }
-                else{
-                    continue;
+                for(int n = 0; n < eNodes.size(); n++){
+                    System.out.println(eNodes.get(n));
                 }
+                System.out.println("Graph " + (i+1) + " Complete, With: " + vSize[i] + " Verticies, and: " + eSize[o] + " Edges\n");
+                vNodes.clear();
+                eNodes.clear();
             }
-            for(i = 0; i < eNodes.size(); i++){
-                System.out.println(eNodes.get(i));
-            }
-            vNodes.clear();
-            eNodes.clear();
-            break;
         }
     }
 }
